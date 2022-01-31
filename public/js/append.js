4f41c1621ab656f6208f319c632422be8fe2df12
fanch__ <averyalexandercarroll08@gmail.com>
2022-01-31 08:07:28 -0500

    css update and added datetime to tasks

--- public/js/append.js
+++ public/js/append.js
@@ -1,12 +1,13 @@
-
 // append task to task list
 var e = document.getElementById("tasks");
 append = function () {
-    var x = document.getElementById("task-title");
-    e.innerHTML += '<h4 class="med-emphasis">' + x.value + '</h4>';
-    x.value = null;
-}
+	var today = new Date();
+	var date = today.toLocaleString("en-US");
+	var x = document.getElementById("task-title");
+	e.innerHTML += '<h4 class="med-emphasis">' + x.value + " | " + date + "</h4>";
+	x.value = null;
+};
 
 clearList = function () {
-    e.innerHTML = null;
-}
+	e.innerHTML = null;
+};
